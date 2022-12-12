f0b5f4a85072d080c537da9a5e34078f0a288b8a
Paulo Clara <pjclara@gmail.com>
2022-12-12 19:58:29 +0000

    atualização edit product

--- backend/app/Http/Requests/UpdateProductRequest.php
+++ backend/app/Http/Requests/UpdateProductRequest.php
@@ -10,18 +10,18 @@ class UpdateProductRequest extends FormRequest
 {
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     public function rules()
     {
         return [
-            'name' => ['required|string|max:255|unique:product,name,' . $this->id . ',id'],
+/*             'name' => ['required|string|max:255|unique:product,name,' . $this->id . ',id'],
             'type' => ['required|' . new Enum(ProductStatusEnum::class)],
             'description' => ['required|string|max:255'],
             'photo_url' => ['required|string|max:255'],
             'price' => ['required|numeric|between:0,999999.99'],
-            'custom' => ['nullable|json'],
+            'custom' => ['nullable|json'], */
         ];
     }
 }
--- frontEnd/src/components/menu/MenuCard.vue
+++ frontEnd/src/components/menu/MenuCard.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="card text-center">
-    <img
-      :src="photoFullUrl"
-      class="mx-auto"
-      alt="https://a.storyblok.com/f/108717/960x640/7fd6d1bd50/7-tipps-fur-gute-food-fotografie1.jpg"
-      height="150"
-      width="150"
-    />
+    <img :src="photoFullUrl" class="mx-auto"
+      alt="https://a.storyblok.com/f/108717/960x640/7fd6d1bd50/7-tipps-fur-gute-food-fotografie1.jpg" height="150"
+      width="150" />
     <div class="card-body">
       <h5 class="card-title">{{ product.name }}</h5>
       <p class="card-text text-start">{{ product.description }}</p>
     </div>
     <div class="card-footer">
       <button class="btn btn-sm btn-outline-primary" @click="cart.value.push(product)">Adicionar</button>
+      <button class="btn btn-info" @click="toogleProductDetail">Edit product</button>
     </div>
+    <product-detail :product="product" :showProduct="showProduct" @close="toogleProductDetail"/>
   </div>
 </template>
 
 <script setup>
-import { computed, inject } from "vue";
+import { computed, inject, ref } from "vue";
 import { useCartStore } from '@/stores/cart.js'
+
+import ProductDetail from '@/components/modals/ProductDetail.vue';
 
 const serverBaseUrl = inject("serverBaseUrl");
 const cart = useCartStore();
@@ -31,13 +31,16 @@ const props = defineProps({
   },
 });
 
+let showProduct = ref(false)
+
+let toogleProductDetail = () => {
+  showProduct.value = !showProduct.value
+}
+
 const photoFullUrl = computed(() => {
   return props.product.photo_url
     ? serverBaseUrl + "/storage/products/" + props.product.photo_url
     : null
 })
 
-const image_alt = null;
-
-let image = computed(() => {});
 </script>
