8a6253eb747d73a778c55c886ac557052dae1313
Paulo Clara <pjclara@gmail.com>
2022-12-12 21:19:06 +0000

    webSocket

--- frontEnd/src/stores/products.js
+++ frontEnd/src/stores/products.js
@@ -4,6 +4,7 @@ import { defineStore } from "pinia";
 export const useProductsStore = defineStore("products", () => {
   const axios = inject("axios");
   const toast = inject("toast");
+  const socket = inject("socket");
 
   const products = ref([]);
 
@@ -27,15 +28,28 @@ export const useProductsStore = defineStore("products", () => {
     try {
       const response = await axios.put(
         "products/" + updateProduct.id,
-        updateProduct        
+        updateProduct
       );
+      socket.emit("updateProduct", response.data.data);
       toast.success(`Produto atualizado com sucesso com successo`);
-      return 1;
+      return products.value
     } catch (error) {}
 
     //updateProjectOnArray(response.data.data)
     //return response.data.data
   }
+
+  function updateProductOnArray(product) {
+    let idx = products.value.findIndex((t) => t.id === product.id);
+    if (idx >= 0) {
+      products.value[idx] = product;
+    }
+  }
+
+  socket.on("updateProduct", (product) => {
+    updateProductOnArray(product);
+    toast.success(`O producto ${product.name} foi atualizado com sucesso.`);
+  });
 
   return { products, loadProducts, updateProduct };
 });
